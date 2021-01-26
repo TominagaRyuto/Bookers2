26c4ab0ff5e955e5e3bd7fe7041978a4b5706d61
TominagaRYuto <esouunnpou@gmail.com>
2021-01-26 19:43:05 +0000

    [finish]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aec77f8e4e39b985ca705fb11a243993b8241b792212a720d3d0023495f54e57330a574f96b5326a458ebe18434e99a2ea0d9ff779a2270696adacdcd573fcce'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  devise_for :users
+  resources :users, only: [:index, :edit, :update, :show]
+  resources :books, only: [:update, :create, :index, :show, :destroy, :edit]
 end
